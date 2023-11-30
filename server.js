65e163a5a36135ed0874ff2d28c896bdcf7532d8
Scotty Jirik <jirikscotty@gmail.com>
2023-11-29 20:06:10 -0600

    I added my get statements

--- server.js
+++ server.js
@@ -12,4 +12,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+    res.json(userInput.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
